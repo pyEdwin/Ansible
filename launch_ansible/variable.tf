bc4543477b4127dfade04499bc1c33ab3afa9b0b
pyEdwin <cyredwin1@gmail.com>
2023-09-20 01:39:46 -0400

    added variable.tf file

---
+++ launch_ansible/variable.tf
@@ -0,0 +1,26 @@
+variable "region" {
+    type = "string"
+    default = "us-east-1"
+}
+
+variable "sg-name" {
+    type = "string"
+    default = "Ansible-sg"
+  
+}
+
+variable "keypair-name" {
+    type = "string"
+    default = "ansible-key"
+  
+}
+# Each object represent a server with multiple attributes(e.g.name, ami...)
+variable "instance_config" {
+    type = map(object({
+      instance_type = string
+      name= string
+      user_data=string
+      ami = string 
+    }))
+  
+}
